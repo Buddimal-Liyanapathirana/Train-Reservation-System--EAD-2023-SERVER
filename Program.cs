0189678a156d054425664f83d545b4ebda4b42f3
Buddimal <buddimalliyanapathirana@gmail.com>
2023-10-17 16:33:46 +0530

    added cors

--- Program.cs
+++ Program.cs
@@ -78,7 +78,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCors(options => options
-    .WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:5000") // Add the origins you need
+    .WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:5000", "https://travelermate.netlify.app") // Add the origins you need
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
